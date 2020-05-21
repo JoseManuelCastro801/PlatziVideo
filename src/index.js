980fa8075aaf2d55988c6b479b5c46f49cfdaf6e
Jose Manuel <josemanuelc801@gmail.com>
2020-05-21 20:09:57 -0300

    Confirguracion inicial de React

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//Configuracion de React , este es el primer archivo detectado
+import React from 'react'
+import ReactDOM from 'react-dom'
+//Se importa el componente Home de la carpeta .\components\
+import Home from './components/Home'
+
+
+//render recibe dos parametro , unos es el componente (ej : Home) y el otro indica donde lo va representar
+ReactDOM.render(<Home/>, document.getElementById(app))
