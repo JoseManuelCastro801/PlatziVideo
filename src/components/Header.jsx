61cc18cb43b1bccba11bf4efb5d11dd9cbb6de53
Jose <josemanuelc801@gmail.com>
2020-05-24 18:06:45 -0300

    Mejora de Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ const Header = () => (
     <header class="header">
         <img class="header__img" src="https://www.flaticon.es/premium-icon/icons/svg/2920/2920847.svg" alt="ImagenHardPoint"/>
         <nav class="header__nav">
-            <p><a href="/">Text1</a></p>
-            <p><a href="/">Text2</a></p>
-            <p><a href="/">Text3</a></p>
-            <input type="text"/>
+            <a class="rainbow-button" href="/" alt=""></a>
+            <a class="rainbow-button" href="/" alt=""></a>
+            <a class="rainbow-button" href="/" alt=""></a>
+            <input type="search" placeholder="Busca tu Archivo" id="Buscador" />
         </nav>
     </header>
 );
