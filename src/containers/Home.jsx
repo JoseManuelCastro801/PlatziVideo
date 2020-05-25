63151b8922e2504f7b096b759ad1ff6274ad6d0b
Jose Manuel <josemanuelc801@gmail.com>
2020-05-25 15:12:41 -0300

    Se agrega buscador con estilos

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
+import Search from '../components/Search'
 
 import '../assets/styles/app.scss'
 
 const Home = () => (
     <div>
     <Header></Header>
-    
+    <Search></Search>
     </div>
 );
 
