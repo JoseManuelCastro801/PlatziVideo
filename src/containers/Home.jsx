1e15d70518d90e8ecfa81bc41e2ff2738b20b8cc
Jose Manuel <josemanuelc801@gmail.com>
2020-05-27 18:35:39 -0300

    Se agerega Hooks al container Home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
@@ -9,36 +9,49 @@ import Footer from '../components/Footer'
 import '../assets/styles/app.scss'
 
 
-const Home = () => (
-    <div>
-    <Header></Header>
-    <Search></Search>
-    <Categories title="Mi lista">
-        <Carousel>
-            <CarouselItem/>
-            <CarouselItem/>
-            <CarouselItem/>
-            <CarouselItem/>
-            <CarouselItem/>
-        </Carousel>
-    </Categories>
-    <Categories title="Recomendados">
-        <Carousel>
-            <CarouselItem/>
-            <CarouselItem/>
-            <CarouselItem/>
-            <CarouselItem/>
-        </Carousel>
-    </Categories>
-    <Categories title="Vistos">
-        <Carousel>
-            <CarouselItem/>
-            <CarouselItem/>
-            <CarouselItem/>
-        </Carousel>
-    </Categories>
-    <Footer/>
-    </div>
-);
+const Home = () =>{
+
+    const [videos , setVideos] = useState([])
+
+    useEffect(() =>{
+        fetch("http://localhost:3000/initalState")
+        .then(response => response.json())
+        .then(data => setVideos(data))
+    },[]);
+
+    console.log(videos)
+
+    return(
+        <div>
+        <Header></Header>
+        <Search></Search>
+        <Categories title="Mi lista">
+            <Carousel>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+            </Carousel>
+        </Categories>
+        <Categories title="Recomendados">
+            <Carousel>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+            </Carousel>
+        </Categories>
+        <Categories title="Vistos">
+            <Carousel>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+            </Carousel>
+        </Categories>
+        <Footer/>
+        </div>
+    );
+} 
 
 export default Home;
