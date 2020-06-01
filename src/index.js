f82eddefbeee7b82a28c29c64569a1bc770b8fa8
Jose Manuel <josemanuelc801@gmail.com>
2020-05-31 23:16:23 -0300

    Se agrega carperas para redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 //Se importa el componente Home de la carpeta .\components\
 import App from './routes/App'
-
+import { Provider } from 'react-redux'
+import { crateStore } from 'redux'
 
 //render recibe dos parametro , unos es el componente (ej : Home) y el otro indica donde lo va representar
-ReactDOM.render(<App/>, document.getElementById('app'))
+ReactDOM.render(
+    <Provider>
+        <App/>
+    </Provider>
+    , document.getElementById('app')
+);
