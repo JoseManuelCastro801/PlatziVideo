ca32a7fe1b84403fb9ab314c38e59d89d2c55f2b
Jose Manuel <josemanuelc801@gmail.com>
2020-06-15 17:06:03 -0300

    Se añade el componente Player

--- src/routes/App.js
+++ src/routes/App.js
@@ -4,7 +4,9 @@ import Home from '../containers/Home'
 import Login from '../containers/Login'
 import Register from '../containers/Register'
 import NotFound from '../containers/NotFound'
+import PLayer from '../components/Player'
 import Layout from '../components/Layout'
+import Player from '../components/Player'
 
 const App = () => (
     <BrowserRouter>
@@ -13,7 +15,7 @@ const App = () => (
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/login" component={Login}/>
                 <Route exact path="/register" component={Register}/>
-                <Route component={NotFound}/>
+                <Route exact path="/Player/:id" component={Player}/>
             </Switch>
         </Layout>
     </BrowserRouter>
