6eb3b3c1ed786e8db2e19a662b473aa5ba4836b8
Jose Manuel <josemanuelc801@gmail.com>
2020-06-15 15:09:16 -0300

    Se añade hooks para el Register

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,22 +1,66 @@
-import React from 'react'
+import React , { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import '../assets/styles/components/Register.scss'
 
-const Register = () => (
+const Register = () =>{
 
+  const [form , setForm] = useState({
+    nombre : "",
+    email : "",
+    password : "",
+  })
+
+  const handleImput = (evento) =>{
+    setForm({
+      ...form,
+      [evento.target.name] : evento.target.value
+    })
+  }
+
+  const handleSubmit = (evento) =>{
+    evento.preventDefault();
+    console.log(form)
+  }
+
+  return(
     <section className="register">
     <section className="register__container">
       <h2>Regístrate</h2>
-      <form className="register__container--form">
-        <input className="input" type="text" placeholder="Nombre"/>
-        <input className="input" type="text" placeholder="Correo"/>
-        <input className="input" type="password" placeholder="Contraseña"/>
+      <form className="register__container--form" onSubmit={handleSubmit}>
+        <input 
+          name="nombre"
+          className="input" 
+          type="text" 
+          placeholder="Nombre"
+          onChange={handleImput}
+        />
+        <input 
+          name="email"
+          className="input" 
+          type="text" 
+          placeholder="Correo"
+          onChange={handleImput}
+        />
+        <input 
+          name="password"
+          className="input" 
+          type="password" 
+          placeholder="Contraseña"
+          onChange={handleImput}
+        />
         <button className="button">Registrarme</button>
+
+        
       </form>
-      <a href="">Iniciar sesión</a>
+      <Link to="/login">
+      Iniciar sesión
+      </Link>
+      
     </section>
   </section>
 
 );
+} 
 
 export default Register;
